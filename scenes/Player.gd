79efad77b436170d41d56183c56887c9a08aafdf
3K <guifenske@gmail.com>
2024-08-02 15:58:10 -0300

    sync the gun shooting animations with the player animations in a better way

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -9,6 +9,9 @@ var isLoadedShoot = false
 var gunNodeScene = preload("res://scenes/gun.tscn")
 var isGunInstantiated = false
 var gun = null
+var flip = 0
+@onready var should_apply_idle_gun_follow = false
+@onready var direction
 
 func instantiate_gun():
 	gun = gunNodeScene.instantiate(PackedScene.GEN_EDIT_STATE_DISABLED)
@@ -16,14 +19,16 @@ func instantiate_gun():
 	get_parent().add_child(gun)
 		
 func _process(delta):
+	gun.look_at(get_global_mouse_position())
+	if should_apply_idle_gun_follow: return
+	flip = 0
+
 	gun.position = position
 	gun.position.x -= 3
 	gun.position.y += 10
-	gun.look_at(get_global_mouse_position())	
 
 func _physics_process(delta):
-	var direction = Input.get_axis("left", "right")
-	
+	direction = Input.get_axis("left", "right")
 	if(!isGunInstantiated):
 		isGunInstantiated = true
 		instantiate_gun()	
@@ -34,7 +39,8 @@ func _physics_process(delta):
 		sprite.flip_h = true	
 				
 	if(Input.is_action_pressed("mouse_right")):
-		isShooting = true			
+		isShooting = true	
+		should_apply_idle_gun_follow = false		
 		if(direction == 0):	sprite.animation = "player_gun_shoot"
 		else: sprite.animation = "walking_shooting"
 			
@@ -43,6 +49,7 @@ func _physics_process(delta):
 		isLoadedShoot = true			
 		direction = 0
 		sprite.animation = "player_loaded_shoot"
+		should_apply_idle_gun_follow = false
 		gun.animation = "loaded_shoot"
 	else:
 		isShooting = false
@@ -50,19 +57,44 @@ func _physics_process(delta):
 		gun.animation = "idle"		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	if Input.is_action_just_pressed("jump") && is_on_floor() && !isLoadedShoot:
 		velocity.y = JUMP_VELOCITY
 
 	if(direction != 0):
+		flip = 0
+		should_apply_idle_gun_follow = false
 		if(isShooting):
 			sprite.animation = "walking_shooting"
 		elif(!isLoadedShoot):	
 			sprite.animation = "walking"
 	else:
-		if(!isShooting && !isLoadedShoot):	sprite.animation = "idle"
+		if(!isShooting && !isLoadedShoot):	
+			sprite.animation = "idle"
+			should_apply_idle_gun_follow = true
 		
 	velocity.x = direction * SPEED
 	# Add the gravity.
 	velocity.y += gravity * delta	
 	
 	move_and_slide()
+		
+
+func _on_sprite_player_frame_changed():
+	if !should_apply_idle_gun_follow: return
+	
+	gun.position = position
+	gun.position.x -= 3
+	gun.position.y += 10
+	
+	if flip == 0:
+		gun.position.y += 5
+		flip += 1
+	else: 
+		flip -= 1
+
+
+func _on_sprite_player_animation_changed():
+	if sprite == null: return
+	if sprite.animation == "walking_shooting" || sprite.animation == "player_gun_shoot":
+		gun.set_frame(0)
+		gun.animation = "shoot"
--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,15 +1,12 @@
 extends CharacterBody2D
 
 @onready var timer = $Timer
-@onready var particles = $BreakParticle
 
 func _physics_process(delta):
 	position += transform.x * 1750 * delta
 
-
 func _on_area_2d_area_entered(area):
 	queue_free()
-
 
 func _on_timer_timeout():
 	queue_free()
